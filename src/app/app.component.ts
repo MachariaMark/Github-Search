3b2f2f78f595c522beaafd173deaeebd828d0983
MachariaMark <official1offmark@gmail.com>
2019-09-29 19:18:07 +0300

    tweaks revolving around ensuring matirialise is up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
   title = 'Github-search';
