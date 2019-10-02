5d6a6155f7035ff4562b9b8190dfdb5075e00c7e
MachariaMark <official1offmark@gmail.com>
2019-10-02 13:12:44 +0300

    added appropriate routing paths and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent  } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { SearchFormComponent } from './search-form/search-form.component'
+import { from } from 'rxjs';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'search', component: SearchFormComponent},
+  { path:'**', component:NotFoundComponent},
 
-const routes: Routes = [];
+  { path: '', redirectTo:"/homepage", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
