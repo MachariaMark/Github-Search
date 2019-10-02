83a6ae64b5690d7ab003ae09dd244fc5bca928d3
MachariaMark <official1offmark@gmail.com>
2019-10-02 13:13:48 +0300

    added all necessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileService } from './services/profile.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HighlightDirective } from './highlight.directive';
+// import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    SearchFormComponent,
+    AboutComponent,
+    NavbarComponent,
+    HomepageComponent,
+    NotFoundComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatSliderModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
